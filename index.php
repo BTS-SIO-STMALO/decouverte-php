bb1e11011c4f55c4100f149416f713ca79fba9f5
mathilderenversade <mathirenversade@gmail.com>
2020-10-26 15:59:16 +0100

    exemple php echo et commentaire

---
+++ index.php
@@ -0,0 +1,21 @@
+<h1>Salut on découvre PHP</h1>
+
+<?php 
+
+// J'affiche un commentaire sur une seule ligne
+
+/*
+Je suis un
+commentaire sur 
+plusieurs lignes, youppppiiii
+*/
+
+// J'affiche un texte 
+echo 'Coucou je suis du php';
+
+// J'affiche du code HTML
+echo '<h2>Coucou je suis du code html généré par php</h2>';
+
+?>
+
+<h2>Ici je suis bien en html</h2>
